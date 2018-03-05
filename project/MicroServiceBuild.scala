215beb46fcf47890ec21aa642f65fff7399f4995
Andrew Chung <andrew.chung@digital.hmrc.gov.uk>
2018-03-05 13:28:30 +0000

    updated bootstrap-play-25 to 1.4.0 (#42)

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -14,7 +14,7 @@ object MicroServiceBuild extends Build with MicroService {
   val compile = Seq(
     ws,
     "org.typelevel" %% "cats" % "0.9.0",
-    "uk.gov.hmrc" %% "bootstrap-play-25" % "1.3.0",
+    "uk.gov.hmrc" %% "bootstrap-play-25" % "1.4.0",
     "com.github.kxbmap" %% "configs" % "0.4.4"
   )
 
