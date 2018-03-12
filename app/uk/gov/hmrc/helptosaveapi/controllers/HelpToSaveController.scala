9880ae902f1958db769133585457e3249b650b30
Andrew Chung <andrew.chung@digital.hmrc.gov.uk>
2018-03-12 10:49:54 +0000

    addded API call metrics (#45)

--- app/uk/gov/hmrc/helptosaveapi/controllers/HelpToSaveController.scala
+++ app/uk/gov/hmrc/helptosaveapi/controllers/HelpToSaveController.scala
@@ -16,10 +16,14 @@
 
 package uk.gov.hmrc.helptosaveapi.controllers
 
+import cats.instances.int._
 import cats.syntax.show._
+import cats.syntax.eq._
+import com.codahale.metrics.Timer
 import com.google.inject.Inject
 import play.api.libs.json.{JsError, JsSuccess}
 import play.api.mvc._
+import uk.gov.hmrc.helptosaveapi.metrics.Metrics
 import uk.gov.hmrc.helptosaveapi.models.{CreateAccountRequest, ErrorResponse}
 import uk.gov.hmrc.helptosaveapi.services.CreateAccountService
 import uk.gov.hmrc.helptosaveapi.util.JsErrorOps._
@@ -29,31 +33,41 @@ import uk.gov.hmrc.play.bootstrap.controller.BaseController
 
 import scala.concurrent.Future
 
-class HelpToSaveController @Inject() (createAccountService: CreateAccountService)
+class HelpToSaveController @Inject() (createAccountService: CreateAccountService, metrics: Metrics)
   extends BaseController with Logging with WithMdcExecutionContext {
 
   val httpHeaderValidator: APIHttpHeaderValidator = new APIHttpHeaderValidator
 
   val createAccountRequestValidator: CreateAccountRequestValidator = new CreateAccountRequestValidator
 
-  def createAccount(): Action[AnyContent] =
-    httpHeaderValidator.validateHeader(e ⇒ BadRequest(ErrorResponse("Invalid HTTP headers in request", e).toJson()))
+  def createAccount(): Action[AnyContent] = {
+    val timer = metrics.apiCallTimer.time()
+
+    httpHeaderValidator.validateHeader{
+      e ⇒
+        updateErrorMetrics(timer)
+        BadRequest(ErrorResponse("Invalid HTTP headers in request", e).toJson())
+    }
       .async { implicit request ⇒
-        validateRequest(request) {
+        validateRequest(request, timer) {
           case CreateAccountRequest(header, body) ⇒
             logger.info(s"Create Account Request has been made with headers: ${header.show}")
             createAccountService.createAccount(body, header.requestCorrelationId).map { response ⇒
+              val _ = timer.stop()
+              if (response.status =!= CREATED | response.status =!= CONFLICT) { metrics.apiCallErrorCounter.inc() }
               Option(response.body).fold[Result](Status(response.status))(Status(response.status)(_))
             }
         }
       }
+  }
 
-  private def validateRequest(request: Request[AnyContent])(f: CreateAccountRequest ⇒ Future[Result]): Future[Result] =
+  private def validateRequest(request: Request[AnyContent], timer: Timer.Context)(f: CreateAccountRequest ⇒ Future[Result]): Future[Result] =
     request.body.asJson.map(_.validate[CreateAccountRequest]) match {
       case Some(JsSuccess(createAccountRequest, _)) ⇒
         createAccountRequestValidator.validateRequest(createAccountRequest)
           .fold(
             { errors ⇒
+              updateErrorMetrics(timer)
               val errorString = s"[${errors.toList.mkString("; ")}]"
               logger.warn(s"Error when validating request: $errorString")
               BadRequest(ErrorResponse("Invalid JSON body in request", errorString).toJson())
@@ -62,14 +76,22 @@ class HelpToSaveController @Inject() (createAccountService: CreateAccountService
           )
 
       case Some(error: JsError) ⇒
+        updateErrorMetrics(timer)
         val errorString = error.prettyPrint()
         logger.warn(s"Could not parse JSON in request body: $errorString")
         BadRequest(ErrorResponse("Could not parse JSON in request", errorString).toJson())
 
       case None ⇒
+        updateErrorMetrics(timer)
         logger.warn("No JSON body found in request")
         BadRequest(ErrorResponse("No JSON found in request body", "").toJson())
     }
+
+  @inline
+  private def updateErrorMetrics(timer: Timer.Context): Unit = {
+    val _ = timer.stop()
+    metrics.apiCallErrorCounter.inc()
+  }
 
 }
 
