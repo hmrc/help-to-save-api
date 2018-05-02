5aec397fa7d57642e23de56cb00d6e6e61cdfa3f
Andy <andrew.chung@digital.hmrc.gov.uk>
2018-05-02 14:50:13 +0100

    hts-1017 - added X-XSS-Protection filter

---
+++ test/uk/gov/hmrc/helptosaveapi/filters/FiltersSpec.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.helptosaveapi.filters
+
+import com.kenshoo.play.metrics.MetricsFilter
+import uk.gov.hmrc.helptosaveapi.util.TestSupport
+import uk.gov.hmrc.play.bootstrap.filters.{AuditFilter, CacheControlFilter, LoggingFilter, MicroserviceFilters}
+
+class FiltersSpec extends TestSupport {
+
+  class EmptyMicroserviceFilters extends MicroserviceFilters(
+    stub[MetricsFilter],
+    stub[AuditFilter],
+    stub[LoggingFilter],
+    stub[CacheControlFilter]
+  )
+
+  val mockMicroServiceFilters = mock[MicroserviceFilters]
+  val mockXSSProtectionFilter = mock[XSSProtectionFilter]
+
+  val filters = new Filters(mockMicroServiceFilters, mockXSSProtectionFilter)
+
+  "Filters" must {
+
+    "include the XSSProtectionFilter" in {
+      filters.filters should contain(mockXSSProtectionFilter)
+    }
+
+  }
+
+}
