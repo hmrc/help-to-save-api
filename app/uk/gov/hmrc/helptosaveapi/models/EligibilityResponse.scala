b02c5609c34be70970c13621835d133fa4b102b9
Suresh Mandalapu <suresh.mandalapu@digital.hmrc.gov.uk>
2018-08-03 14:45:46 +0100

    fix previous feedback

--- app/uk/gov/hmrc/helptosaveapi/models/EligibilityResponse.scala
+++ app/uk/gov/hmrc/helptosaveapi/models/EligibilityResponse.scala
@@ -44,9 +44,9 @@ object EligibilityResponse {
       json.asOpt[ApiEligibilityResponse] match {
         case Some(eligibilityResponse) ⇒ JsSuccess(eligibilityResponse)
         case None ⇒
-          (json \ "accountExists").asOpt[String] match {
-            case Some(data) ⇒ JsSuccess(AccountAlreadyExists())
-            case None       ⇒ JsError(s"couldn't parse eligibility json from mongo, json=$json")
+          json.\ ("accountExists").as[Boolean] match {
+            case true  ⇒ JsSuccess(AccountAlreadyExists())
+            case false ⇒ JsError(s"couldn't parse eligibility json from mongo, json=$json")
           }
       }
     }
