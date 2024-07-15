1622020507cc8c3268fcb9c4411db53027485f85
TomRafferty <86563641+TomRafferty@users.noreply.github.com>
2024-07-15 09:35:15 +0100

    DLS-10610 - removed @nowarn annotation

--- app/uk/gov/hmrc/helptosaveapi/controllers/HelpToSaveController.scala
+++ app/uk/gov/hmrc/helptosaveapi/controllers/HelpToSaveController.scala
@@ -37,7 +37,6 @@ import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
 
 import java.util.UUID
-import scala.annotation.nowarn
 import scala.concurrent.{ExecutionContext, Future}
 
 class HelpToSaveController @Inject() (
@@ -65,7 +64,6 @@ class HelpToSaveController @Inject() (
     case _: ApiBackendError    => InternalServerError(Json.toJson(e))
   }
 
-  @nowarn //silence warning of deprecated v2AuthProviderId
   def createAccount(): Action[AnyContent] = authorised(v2AuthProviderId) { implicit request => credentials =>
     def handleResult(result: Either[ApiError, CreateAccountSuccess]): Result =
       result match {
@@ -111,7 +109,6 @@ class HelpToSaveController @Inject() (
     }
   }
 
-  @nowarn //silence warning of deprecated v2AuthProviderId
   def checkEligibilityDeriveNino(): Action[AnyContent] = authorised(v2AuthProviderId) {
     implicit request => credentials =>
       val correlationId = UUID.randomUUID()
@@ -140,7 +137,6 @@ class HelpToSaveController @Inject() (
 
   }
 
-  @nowarn //silence warning of deprecated v2AuthProviderId
   def checkEligibility(urlNino: String): Action[AnyContent] = authorised(v2AuthProviderId) {
     implicit request => credentials =>
       val correlationId = UUID.randomUUID()
