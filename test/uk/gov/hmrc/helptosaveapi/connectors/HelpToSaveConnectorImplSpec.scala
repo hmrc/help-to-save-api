98015f7309623dfac643cde187b0d34455d739d6
ShivangiShah42 <177633426+ShivangiShah42@users.noreply.github.com>
2024-09-10 11:53:13 +0100

    DLS-10782 updated connector test to wiremock

--- test/uk/gov/hmrc/helptosaveapi/connectors/HelpToSaveConnectorImplSpec.scala
+++ test/uk/gov/hmrc/helptosaveapi/connectors/HelpToSaveConnectorImplSpec.scala
@@ -16,26 +16,58 @@
 
 package uk.gov.hmrc.helptosaveapi.connectors
 
-import org.scalacheck.{Arbitrary, Gen}
-import org.scalatest.EitherValues
-import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
-import play.api.libs.json.{JsString, Json}
+import com.typesafe.config.ConfigFactory
+import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.libs.json.Json
+import play.api.test.Helpers.{await, defaultAwaitTimeout}
+import play.api.{Application, Configuration}
 import uk.gov.hmrc.helptosaveapi.connectors.HelpToSaveConnectorImpl.CreateAccountInfo
 import uk.gov.hmrc.helptosaveapi.models.ValidateBankDetailsRequest
-import uk.gov.hmrc.helptosaveapi.models.createaccount.CreateAccountBody
-import uk.gov.hmrc.helptosaveapi.util.{DataGenerators, MockPagerDuty, TestSupport, base64Encode}
-import uk.gov.hmrc.http.HttpResponse
+import uk.gov.hmrc.helptosaveapi.util.DataGenerators.{random, validCreateAccountBodyGen}
+import uk.gov.hmrc.helptosaveapi.util.{WireMockMethods, base64Encode}
+import uk.gov.hmrc.http.test.WireMockSupport
+import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse}
 
 import java.util.UUID
+import scala.concurrent.ExecutionContext
 
 // scalastyle:off magic.number
 class HelpToSaveConnectorImplSpec
-    extends TestSupport with MockPagerDuty with EitherValues with HttpSupport with ScalaCheckPropertyChecks {
+    extends AnyWordSpec with WireMockMethods with WireMockSupport with GuiceOneAppPerSuite {
 
-  val connector = new HelpToSaveConnectorImpl(fakeApplication.configuration, mockHttp)()
+  val (desBearerToken, desEnvironment) = "token" -> "environment"
+
+  private val config = Configuration(
+    ConfigFactory.parseString(
+      s"""
+         |microservice {
+         |  services {
+         |      help-to-save {
+         |      protocol = http
+         |      host     = $wireMockHost
+         |      port     = $wireMockPort
+         |    }
+         |  }
+         |}
+         |
+         |des {
+         |  bearer-token = $desBearerToken
+         |  environment  = $desEnvironment
+         |}
+         |""".stripMargin
+    )
+  )
+  override def fakeApplication(): Application = new GuiceApplicationBuilder().configure(config).build()
+
+  val connector: HelpToSaveConnector = app.injector.instanceOf[HelpToSaveConnectorImpl]
+  implicit val hc: HeaderCarrier = HeaderCarrier()
+  implicit lazy val ec: ExecutionContext = app.injector.instanceOf[ExecutionContext]
+  private val emptyJsonBody = "{}"
 
   "The HelpToSaveConnectorImpl" when {
-
     val nino = "AE123456C"
     val systemId = "systemId"
     val correlationId = UUID.randomUUID()
@@ -44,36 +76,44 @@ class HelpToSaveConnectorImplSpec
     "creating an account" must {
 
       "call the correct url and return the response as is" in {
-        implicit val createAccountBodyArb: Arbitrary[CreateAccountBody] =
-          Arbitrary(DataGenerators.validCreateAccountBodyGen)
-        implicit val correlationIdArb: Arbitrary[UUID] = Arbitrary(Gen.uuid)
-
-        forAll { (body: CreateAccountBody, correlationId: UUID, clientCode: String, status: Int, response: String) =>
-          mockPost(
-            "http://localhost:7001/help-to-save/create-account",
-            CreateAccountInfo(body, 8, clientCode),
-            Map("X-Correlation-ID" -> correlationId.toString)
-          )(Some(HttpResponse(status, JsString(response), Map.empty[String, Seq[String]])))
-          val result = await(connector.createAccount(body, correlationId, clientCode, 8))
-          result.status shouldBe status
-          result.json shouldBe JsString(response)
+        List(
+          HttpResponse(200, emptyJsonBody),
+          HttpResponse(400, emptyJsonBody),
+          HttpResponse(401, emptyJsonBody),
+          HttpResponse(403, emptyJsonBody),
+          HttpResponse(500, emptyJsonBody),
+          HttpResponse(502, emptyJsonBody),
+          HttpResponse(503, emptyJsonBody)
+        ).foreach { httpResponse =>
+          val createAccountBody = random(validCreateAccountBodyGen)
+          when(
+            POST,
+            "/help-to-save/create-account",
+            Map.empty,
+            Map("X-Correlation-ID" -> correlationId.toString),
+            Some(Json.toJson(CreateAccountInfo(createAccountBody, 8, "1234")).toString())
+          ) thenReturn (httpResponse.status, httpResponse.body)
+          val result = await(connector.createAccount(createAccountBody, correlationId, "1234", 8))
+          result.status shouldBe httpResponse.status
+          result.body shouldBe httpResponse.body
         }
       }
-
     }
 
     "handling eligibility requests" must {
 
-      val eligibilityUrl = "http://localhost:7001/help-to-save/eligibility-check"
-
+      val eligibilityUrl = "/help-to-save/eligibility-check"
       val eligibilityJson = """{"result": "eligible","resultCode": 1,"reason": "receiving UC","reasonCode": 5}"""
       val json = Json.parse(eligibilityJson)
 
       "call correct url and return the response as is" in {
-
-        mockGet(eligibilityUrl, Map("nino" -> nino), headers)(
-          Some(HttpResponse(200, json, Map.empty[String, Seq[String]]))
-        )
+        val httpResponse = HttpResponse(200, json, Map.empty[String, Seq[String]])
+        when(
+          GET,
+          eligibilityUrl,
+          Map("nino" -> nino),
+          headers
+        ) thenReturn (httpResponse.status, httpResponse.body)
         val result = await(connector.checkEligibility(nino, correlationId))
         result.status shouldBe 200
         result.json shouldBe json
@@ -82,7 +122,7 @@ class HelpToSaveConnectorImplSpec
 
     "handling get account requests" must {
 
-      val getAccountUrl = s"http://localhost:7001/help-to-save/$nino/account"
+      val getAccountUrl = s"/help-to-save/$nino/account"
       val account =
         """{
           |"accountNumber":"1100000000001",
@@ -109,10 +149,13 @@ class HelpToSaveConnectorImplSpec
       val json = Json.parse(account)
 
       "call the correct url and return the response as is" in {
-
-        mockGet(getAccountUrl, Map("systemId" -> systemId, "correlationId" -> correlationId.toString), headers)(
-          Some(HttpResponse(200, json, Map.empty[String, Seq[String]]))
-        )
+        val httpResponse = HttpResponse(200, json, Map.empty[String, Seq[String]])
+        when(
+          GET,
+          getAccountUrl,
+          Map("systemId" -> systemId, "correlationId" -> correlationId.toString),
+          headers
+        ) thenReturn (httpResponse.status, httpResponse.body)
         val result = await(connector.getAccount(nino, systemId, correlationId))
         result.status shouldBe 200
         result.json shouldBe json
@@ -120,31 +163,36 @@ class HelpToSaveConnectorImplSpec
     }
 
     "handling store email requests" must {
-
-      val storeEmailUrl = "http://localhost:7001/help-to-save/store-email"
-
+      val storeEmailUrl = "/help-to-save/store-email"
       "call the correct url and return the response as is" in {
         val email = base64Encode("email@email.com")
+        val httpResponse = HttpResponse(200, "")
+        when(
+          GET,
+          storeEmailUrl,
+          Map("email" -> email, "nino" -> nino),
+          headers
+        ) thenReturn (httpResponse.status, httpResponse.body)
 
-        mockGet(storeEmailUrl, Map("email" -> email, "nino" -> nino), headers)(Some(HttpResponse(200, "")))
         val result = await(connector.storeEmail(email, nino, correlationId))
         result.status shouldBe 200
       }
-
     }
 
     "validating bank details" must {
-
       "return http response as it is to the caller" in {
-        val response = HttpResponse(200, Json.parse("""{"isValid":true}"""), Map.empty[String, Seq[String]])
-        mockPost(
-          "http://localhost:7001/help-to-save/validate-bank-details",
-          ValidateBankDetailsRequest(nino, "123456", "02012345"),
-          Map.empty
-        )(Some(response))
-        await(connector.validateBankDetails(ValidateBankDetailsRequest(nino, "123456", "02012345"))) shouldBe response
+        val httpResponse = HttpResponse(200, Json.parse("""{"isValid":true}"""), Map.empty[String, Seq[String]])
+        val request = ValidateBankDetailsRequest(nino, "123456", "02012345")
+        when(
+          POST,
+          "/help-to-save/validate-bank-details",
+          headers = Map.empty,
+          body = Some(Json.toJson(request).toString())
+        ) thenReturn (httpResponse.status, httpResponse.body)
+        val result = await(connector.validateBankDetails(request))
+        result.status shouldBe httpResponse.status
+        result.body shouldBe httpResponse.body
       }
     }
-
   }
 }
